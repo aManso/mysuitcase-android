07b675714f15b936f52aeebe993948025dca6ebf
Alejandro Manso Gonzalez <Alejandro.Manso@cognizant.com>
2015-11-18 13:33:59 +0100

    minor

--- app/js/controllers/chooseOptionsController.js
+++ app/js/controllers/chooseOptionsController.js
@@ -21,7 +21,7 @@ suitcaseApp.controller('chooseOptionsController',function chooseOptionsControlle
         if($scope.checkForm(form)){
             $log.info("valid form");
             currentTravelService.setTravelOptions($scope.travel);
-            cookieService.setCookies('travelOptions', $scope.travel);
+            cookieService.setCookie('travelOptions', $scope.travel);
             $location.path(PATHS.makeSuitcase)
         }
     }
